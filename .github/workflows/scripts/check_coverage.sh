e3d864e9b22013ea54d424ab0912ec661b188930
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-06-12 10:55:18 +0300

    add env vars

--- .github/workflows/scripts/check_coverage.sh
+++ .github/workflows/scripts/check_coverage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -xeu
 
-coverage=$(grep -m 1 '<abbr title' coverage/index.html | grep -o "[0-9.]*" | tail -1)
-failure=$(awk 'BEGIN{ print '"$coverage"'<'"$COV_THRESHOLD"' }')
+coverage = $(grep -m 1 '<abbr title' coverage/index.html | grep -o "[0-9.]*" | tail -1)
+failure = $(awk 'BEGIN{ print '"$coverage"'<'"$COVERAGE_THRESHOLD"' }')
 if [ "$failure" -eq "1" ]; then
-    echo "Coverage has failed with $coverage% instead of at least $COV_THRESHOLD%."
+    echo "Coverage has failed with $coverage% instead of at least $COVERAGE_THRESHOLD%."
     exit 1
 else
     echo "Coverage has successfully passed with $coverage%."
