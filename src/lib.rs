15cc56222b92812c73f0be59f6a539428dc13d0c
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-06-12 11:42:25 +0300

    report

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,3 @@
-pub fn caesar_encrypt(message: &str, shift: u8) -> String {
-    let mut encrypted_message = String::new();
-
-    for c in message.chars() {
-        let encrypted_char = match c {
-            'A'..='Z' => (((c as u8 - b'A') + shift) % 26 + b'A') as char,
-            'a'..='z' => (((c as u8 - b'a') + shift) % 26 + b'a') as char,
-            _ => c,
-        };
-
-        encrypted_message.push(encrypted_char);
-    }
-
-    encrypted_message
-}
-
-pub fn caesar_decrypt(message: &str, shift: u8) -> String {
-    caesar_encrypt(message, 26 - (shift % 26))
-}
-
 pub fn atbash_encrypt(message: &str) -> String {
     let mut encrypted_message = String::new();
 
@@ -38,6 +18,46 @@ pub fn atbash_decrypt(message: &str) -> String {
     atbash_encrypt(message)
 }
 
+pub fn caesar_encrypt(message: &str, shift: u8) -> String {
+    let mut encrypted_message = String::new();
+
+    for c in message.chars() {
+        let encrypted_char = match c {
+            'A'..='Z' => (((c as u8 - b'A') + shift) % 26 + b'A') as char,
+            'a'..='z' => (((c as u8 - b'a') + shift) % 26 + b'a') as char,
+            _ => c,
+        };
+
+        encrypted_message.push(encrypted_char);
+    }
+
+    encrypted_message
+}
+
+pub fn caesar_decrypt(message: &str, shift: u8) -> String {
+    caesar_encrypt(message, 26 - (shift % 26))
+}
+
+pub fn rot13_encrypt(message: &str) -> String {
+    let mut encrypted_message = String::new();
+
+    for c in message.chars() {
+        let encrypted_char = match c {
+            'A'..='M' | 'a'..='m' => ((c as u8) + 13) as char,
+            'N'..='Z' | 'n'..='z' => ((c as u8) - 13) as char,
+            _ => c,
+        };
+
+        encrypted_message.push(encrypted_char);
+    }
+
+    encrypted_message
+}
+
+pub fn rot13_decrypt(message: &str) -> String {
+    rot13_encrypt(message)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,18 +67,6 @@ mod tests {
         assert_eq!(caesar_encrypt("hello world", 3), "khoor zruog");
     }
 
-    #[test]
-    fn test_caesar_decrypt() {
-        assert_eq!(caesar_decrypt("khoor zruog", 3), "hello world");
-    }
+    // add more tests here
 
-    #[test]
-    fn test_atbash_encrypt() {
-        assert_eq!(atbash_encrypt("hello world"), "svool dliow");
-    }
-
-    #[test]
-    fn test_atbash_decrypt() {
-        assert_eq!(atbash_decrypt("svool dliow"), "hello world");
-    }
 }
