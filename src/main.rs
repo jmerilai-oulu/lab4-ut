9fc8fdff8db2684b594b6bf66c1fa80cb72fa6df
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-06-12 11:01:54 +0300

    add main content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+
+pub fn caesar_encrypt(message: &str, shift: u8) -> String {
+    let mut encrypted_message = String::new();
+
+    for c in message.chars() {
+        let encrypted_char = match c {
+            'A'..='Z' => (((c as u8 - b'A') + shift) % 26 + b'A') as char,
+            'a'..='z' => (((c as u8 - b'a') + shift) % 26 + b'a') as char,
+            _ => c,
+        };
+
+        encrypted_message.push(encrypted_char);
+    }
+
+    encrypted_message
+}
+
+pub fn caesar_decrypt(message: &str, shift: u8) -> String {
+    caesar_encrypt(message, 26 - (shift % 26))
+}
+
+pub fn atbash_encrypt(message: &str) -> String {
+    let mut encrypted_message = String::new();
+
+    for c in message.chars() {
+        let encrypted_char = match c {
+            'A'..='Z' => ('Z' as u8 - (c as u8 - 'A' as u8)) as char,
+            'a'..='z' => ('z' as u8 - (c as u8 - 'a' as u8)) as char,
+            _ => c,
+        };
+
+        encrypted_message.push(encrypted_char);
+    }
+
+    encrypted_message
+}
+
+pub fn atbash_decrypt(message: &str) -> String {
+    atbash_encrypt(message)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_caesar_encrypt() {
+        assert_eq!(caesar_encrypt("hello world", 3), "khoor zruog");
+    }
 }
