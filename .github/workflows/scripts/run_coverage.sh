cc4da8f2c0754799bb9c578e081c1032b34f402f
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-06-12 11:51:13 +0300

    fix

--- .github/workflows/scripts/run_coverage.sh
+++ .github/workflows/scripts/run_coverage.sh
@@ -2,4 +2,4 @@
 set -xeu
 
 RUSTFLAGS="-Zinstrument-coverage" cargo test --lib
-grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing -o ./coverage/ --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
+grcov . --binary-path ./target/debug/ -s . -t html --branch --ignore-not-existing -o ./ --excl-start "#\[cfg\(test\)\]" --keep-only **/src/**/*
