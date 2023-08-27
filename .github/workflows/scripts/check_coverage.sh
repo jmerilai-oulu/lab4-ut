b582c5216b7e9e072de0b6d969cd2a1e945de203
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-08-28 02:14:49 +0300

    90

--- .github/workflows/scripts/check_coverage.sh
+++ .github/workflows/scripts/check_coverage.sh
@@ -2,11 +2,11 @@
 
 coverage_output=$(cargo tarpaulin)
 
-coverage_percentage=$(echo "$coverage_output" | grep -oP '100\.00%' | awk '{print $1}')
+coverage_percentage=$(echo "$coverage_output" | grep -oP '(\d+\.\d+)%' | awk '{print $1}')
 
 coverage_float=$(echo "$coverage_percentage" | sed 's/%//')
 
-if (( $(echo "$coverage_float >= 95" | bc -l) )); then
+if (( $(echo "$coverage_float >= 90" | bc -l) )); then
   echo "Coverage is over 90%: $coverage_percentage"
 else
   echo "Coverage is below 90%: $coverage_percentage"
